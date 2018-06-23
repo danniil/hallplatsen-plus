dcb3ec144595d8c71c5f9ccdba092ffa58a6cf55
Daniel N <nilssond1@gmail.com>
2018-06-24 01:05:21 +0200

    gotta love gitignore, want to ignore changes not delete

---
+++ src/config/cred.js
@@ -0,0 +1,13 @@
+export const cred = {
+  NODE_ENV: 'development',
+
+  DB_HOST: 'localhost',
+  DB_KEY: 'theonering',
+  DB_TOKEN: 'toliken',
+  DB_SECRET: 'fightclub',
+  DB_USER: 'root',
+  DB_PASSWORD: 'baconpancakes',
+  DB_NAME: 'tree_fort'
+};
+
+//these are legit AF
